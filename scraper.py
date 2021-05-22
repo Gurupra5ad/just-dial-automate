9e547b97933be1faa320759e0d6e5077a97221a7
Guru <guruprasad2511@gmail.com>
2021-05-22 22:30:55 +0530

    Changed scrape file, added basic gui and csv extracted

--- scraper.py
+++ scraper.py
@@ -1,42 +1,140 @@
+from bs4 import BeautifulSoup
+import urllib
+import csv
 import requests
+import sys
 import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from urllib.request import urlopen
+import pandas as pd
+from pandas import DataFrame
 
-#assigning the browser and getting the site
-browser = webdriver.Chrome('/home/disciple/chromedriver')
-wait = WebDriverWait(browser,60)
-browser.get("https://www.justdial.com/")
+# browser = webdriver.Chrome()
 
-# Login automation ----------------------------------------------------------------------------------------
+def innerHTML(element):
+    return element.decode_contents(formatter="html")
 
-time.sleep(3)
-#click login button on top nav bar
-wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/header/section/div/div[2]/ul/li[4]/ul/li[1]/a'))).click() 
-time.sleep(3)
-#sending in name and phone number keys for login automation
-#sending in the username 
-login_name = browser.find_element_by_xpath('//*[@id="lgn_name"]')
-login_name.send_keys("shreedhar")
-time.sleep(3)
-#sending in the phone number
-login_number = browser.find_element_by_xpath('//*[@id="lgn_mob"]')
-login_number.send_keys("9994987315") 
-#clicking get otp button
-wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="lgn_smtn"]'))).click() 
-#going inside otp page
-otp = input("-------------------Enter your 6 digit OTP--------------------- ")
-time.sleep(3)
-for i in range(6):
-    #targetting each otp input dash
-    login_otp = browser.find_element_by_xpath(f'//*[@id="pin_{i}"]')
-    login_otp.send_keys(otp[i]) 
-#click otp submit button
-wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="otp_submit"]'))).click() 
-#redirecting to the main page after login
-time.sleep(20)
-browser.get('https://www.justdial.com/')
+def get_name(body):
+	return body.find('span', {'class':'jcn'}).a.string
+
+def which_digit(html):
+    mappingDict={'icon-ji':9,
+                'icon-lk':8,
+                'icon-nm':7,
+                'icon-po':6,
+                'icon-rq':5,
+                'icon-ts':4,
+                'icon-vu':3,
+                'icon-wx':2,
+                'icon-yz':1,
+                'icon-acb':0,
+                }
+    return mappingDict.get(html,'')
+
+def get_phone_number(body):
+    i=0
+    phoneNo = "No Number!"
+    try:
+            
+        for item in body.find('p',{'class':'contact-info'}):
+            i+=1
+            if(i==2):
+                phoneNo=''
+                try:
+                    for element in item.find_all(class_=True):
+                        classes = []
+                        classes.extend(element["class"])
+                        phoneNo+=str((which_digit(classes[1])))
+                except:
+                    pass
+    except:
+        pass
+    body = body['data-href']
+    # browser.get(body)
+    # html = browser.page_source
+    soup = BeautifulSoup(body, 'html.parser')
+    for a in soup.find_all('a', {"id":"whatsapptriggeer"} ):
+        # print (a)
+        phoneNo = str(a['href'][-10:])
+
+
+    return phoneNo
+
+def get_tags(body):
+	return body.find('span', {'class':'margin0'}).text.strip()
+
+def get_address(body):
+	return body.find('span', {'class':'mrehover'}).text.strip()
+
+
+def get_location(body):
+	text = body.find('a', {'class':'rsmap'})
+	if text == None:
+		return
+	text_list = text['onclick'].split(",")
+	
+	latitutde = text_list[3].strip().replace("'", "")
+	longitude = text_list[4].strip().replace("'", "")
+	
+	return latitutde + ", " + longitude
+
+page_number = 1
+service_count = 1
+
+fields = ['Name', 'Phone', 'Address', 'Location']
+
+
+out_file = open('data.csv','w')
+csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields)
+
+url_input = input("Enter the url to scrape : ")
+# Write fields first
+csvwriter.writerow(dict((fn,fn) for fn in fields))
+
+while True:
+
+	# Check if reached end of result
+	if page_number > 5:
+		break
+	session = requests.Session()
+	session.headers.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"})
+	url=url_input+"-%s" % (page_number)
+    # url="https://www.justdial.com/Bangalore/Car-Repair-Services/nct-10976632-%s" % (page_number)
+
+
+	r = session.get(url); 
+
+	soup = BeautifulSoup(r.content, "lxml")
+	services = soup.find_all('li', {'class': 'cntanr'})
+
+
+	# Iterate through the 10 results in the page
+	for service_html in services:
+
+		# Parse HTML to fetch data
+		dict_service = {}
+		name = get_name(service_html)
+		phone = get_phone_number(service_html)
+		address = get_address(service_html)
+		location = get_location(service_html)
+		if name != None:
+			dict_service['Name'] = name
+		if phone != None:
+			print('getting phone number')
+			dict_service['Phone'] = phone
+		if address != None:
+			dict_service['Address'] = address
+		if location != None:
+			dict_service['Location'] = location
+
+		# Write row to CSV
+		csvwriter.writerow(dict_service)
+
+
+		data = {}
+
+		print("#" + str(service_count) + " " , dict_service)
+		service_count += 1
+
+	page_number += 1
+
+out_file.close()
